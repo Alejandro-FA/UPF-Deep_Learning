2810651eb4b1937ce4d86776772cfd0d7738dc9a
Alejandro-FA <61871580+Alejandro-FA@users.noreply.github.com>
2023-04-25 11:11:49 +0200

    Ex1 finished and ex 2 almost finished
    Co-authored-by: marcagve <marcagve18@users.noreply.github.com>
    Co-authored-by: Andreu <AndreuG01@users.noreply.github.com>

--- P1/ex1.py
+++ P1/ex1.py
@@ -1,30 +1,52 @@
 import pandas as pd 
-import matplotlib.pyplot as plt #  matplotlib for visualization
 import numpy as np
-
+import matplotlib.pyplot as plt #  matplotlib for visualization
 
 data_path = 'P1/Data/'
-results_path = 'P1/Results'
+results_path = 'P1/Results/'
+
+seed_value = 10
+np.random.seed(seed_value)
 
 # Load training and testing data
 train_data = pd.read_csv(data_path + "train.csv")
 test_data = pd.read_csv(data_path + "test.csv")
 
-X_train = train_data[["X", "Y"]].to_numpy()
-C_train = train_data["C"].to_numpy().reshape(100, 1)
+X_train = train_data[["X", "Y"]].to_numpy(dtype=np.double)
+C_train = train_data["C"].to_numpy(dtype=np.int32).reshape(-1, 1)
 
+X_test = test_data[["X", "Y"]].to_numpy(dtype=np.double)
+C_test = test_data["C"].to_numpy(dtype=np.int32).reshape(-1, 1)
 
-# Visualize data
+########################################### Cell 2
+
+# Visualize original data
+fig1 = plt.figure()
 plt.title("Training data", fontsize=14, fontweight="bold")
-plt.scatter(train_data.X, train_data.Y, c=train_data.C)
+plt.scatter(X_train[:,0], X_train[:,1], c=C_train)
 plt.xlabel("x")
 plt.ylabel("y")
 plt.show()
 
 # Normalize dataset
-X_train = X_train / np.amax(X_train, axis=0)
+def normalize(X):
+    mu = np.mean(X, axis=0)
+    sigma = np.std(X, axis=0)
+    return (X - mu) / sigma
+    
+X_train_norm = normalize(X_train)
+X_test_norm = normalize(X_test)
 
-# MLP Class:
+# Visualize normalized data
+fig2 = plt.figure()
+plt.title("Normalized training data", fontsize=14, fontweight="bold")
+plt.scatter(X_train_norm[:,0], X_train_norm[:,1], c=C_train)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
+
+########################################### Cell 3
+
 class MLP(object):
     def __init__(self,inputNode=2,hiddenNode = 3, outputNode=1):        
         #Define MLP hyper-parameters
@@ -36,12 +58,16 @@ class MLP(object):
         self.W1 = np.random.randn(self.inputLayerSize,self.hiddenLayerSize)
         self.W2 = np.random.randn(self.hiddenLayerSize,self.outputLayerSize)
         
-    def forward(self, X):
+    def forward(self, X, update=True):
         #Compute inputs from outputs
-        self.z2 = np.dot(X, self.W1)
-        self.a2 = self.sigmoid(self.z2)
-        self.z3 = np.dot(self.a2, self.W2)
-        yHat = self.sigmoid(self.z3) 
+        z2 = np.dot(X, self.W1)
+        a2 = self.sigmoid(z2)
+        z3 = np.dot(a2, self.W2)
+        if update:
+            self.z2 = z2
+            self.a2 = a2
+            self.z3 = z3
+        yHat = self.sigmoid(z3) 
         return yHat
         
     def sigmoid(self, z):
@@ -53,23 +79,176 @@ class MLP(object):
         #Gradient of sigmoid
         return np.exp(-z)/((1+np.exp(-z))**2)
     
-    def loss(self, yHat, y):
-        #Compute loss for given X,y, use weights already stored in class.
-        J = 0.5*sum((y-yHat)**2)
+    def loss(self, yHat, y): # Cross entropy
+        # Compute loss for given X,y, use weights already stored in class.
+        H = np.multiply(-y, np.log(yHat)) - np.multiply((1-y), np.log(1-yHat))
+        J = 1/y.size * np.sum(H) # CrossEntropy
         return J
+
+    def lossPrime(self, yHat, y): # Cross entropy
+        dJdyHat = np.divide(-y, yHat) + np.divide(1-y, 1-yHat)
+        return dJdyHat
         
     # Derivative of parameters
     def backward(self,X, yHat, y):
         self.yHat = yHat
         
-        delta3 = np.multiply(-(y-self.yHat), self.sigmoidPrime(self.z3))
+        delta3 = np.multiply(self.lossPrime(yHat, y), self.sigmoidPrime(self.z3))
         dJdW2 = np.dot(self.a2.T, delta3)
         
         delta2 = np.dot(delta3, self.W2.T)*self.sigmoidPrime(self.z2)
         dJdW1 = np.dot(X.T, delta2)  
         return dJdW1,dJdW2
 
-def train(NN,X, y,epoch = 10000,lr = .1):
+########################################### Cell 4
+
+# Train and visualize the decision boundary
+def train(NN,X_train, X_test, y_train, y_test, epoch = 10000, lr = .1, verbose=False):
+    train_list_loss = []
+    test_list_loss = []
+    
+    for i in range(epoch):
+        #Compute derivative with respect to W and W2 for a given X and y:
+        
+        #TODO: ask the teacher how to do this properly
+        yHat_test = NN.forward(X_test, update=False)
+        yHat_train = NN.forward(X_train)
+
+        
+        gradW1,gradW2 = NN.backward(X_train,yHat_train,y_train)
+        #now update the weight using gradient descent
+        NN.W1 -= gradW1 * lr
+        NN.W2 -= gradW2 * lr
+        
+        if i%100 == 0 :  
+            train_loss = NN.loss(yHat_train, y_train)
+            test_loss = NN.loss(yHat_test, y_test)
+            if verbose:
+                print('Train Loss {}={}'.format(i,train_loss))
+                print('Test Loss {}={}'.format(i,test_loss))
+
+            train_list_loss.append(train_loss)
+            test_list_loss.append(test_loss)
+        
+    return train_list_loss, test_list_loss
+
+
+NN = MLP(hiddenNode=20)
+#Train network with the data:
+train_list_loss, test_list_loss = train(NN, X_train_norm, X_test_norm, C_train, C_test, epoch=10000, lr=0.01)
+
+########################################### Cell 5
+
+def classify(NN, X):
+    yHat = NN.forward(X)
+    return yHat.round().astype(int)
+
+# Plot the training data as a scatter plot (for reference)
+fig3 = plt.figure()
+class_0_points = X_train_norm[C_train[:,0] == 0]
+class_1_points = X_train_norm[C_train[:,0] == 1]
+plt.scatter(class_0_points[:, 0], class_0_points[:, 1], color='blue', label='True class 0')
+plt.scatter(class_1_points[:, 0], class_1_points[:, 1], color='red', label='True class 1')
+
+def boundary_points(X, NN):
+    # Plot the decision boundary
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01), np.arange(y_min, y_max, 0.01))
+    grid_points = np.c_[xx.ravel(), yy.ravel()]
+    Z = classify(NN, grid_points)
+    Z = Z.reshape(xx.shape)
+    return xx, yy, Z
+
+xx_train, yy_train, Z = boundary_points(X_train_norm, NN) 
+plt.contourf(xx_train, yy_train, Z, alpha=0.3, cmap='coolwarm')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Decision boundary for the training dataset', fontsize=14, fontweight="bold")
+plt.legend()
+
+plt.show()
+
+########################################### Cell 6
+
+fig4 = plt.figure()
+plt.title('Loss evolution', fontsize=14, fontweight="bold")
+plt.plot(train_list_loss, color="black", label="Train loss")
+plt.plot(test_list_loss, color="red", label="Test loss")
+plt.xlabel('Iterations')
+plt.ylabel('Loss Val')
+plt.legend()
+plt.ylim(0, None)
+plt.grid()
+plt.show()
+
+
+########################################### Cell 7
+# Classify the data in test.csv using the trained MLP
+
+
+output_test = classify(NN, X_test_norm)
+                                
+true0_pred0_points = X_test_norm[(C_test[:,0] == 0) & (output_test[:,0] == 0)]
+true0_pred1_points = X_test_norm[(C_test[:,0] == 0) & (output_test[:,0] == 1)]
+true1_pred1_points = X_test_norm[(C_test[:,0] == 1) & (output_test[:,0] == 1)]
+true1_pred0_points = X_test_norm[(C_test[:,0] == 1) & (output_test[:,0] == 0)]
+
+fig5 = plt.figure()
+plt.scatter(true0_pred0_points[:, 0], true0_pred0_points[:, 1], color='blue', label='True class 0, Predicted class 0')
+plt.scatter(true0_pred1_points[:, 0], true0_pred1_points[:, 1], color='cyan', label='True class 0, Predicted class 1')
+plt.scatter(true1_pred1_points[:, 0], true1_pred1_points[:, 1], color='red', label='True class 1, Predicted class 1')
+plt.scatter(true1_pred0_points[:, 0], true1_pred0_points[:, 1], color='orange', label='True class 1, Predicted class 0')
+
+xx_test, yy_test, Z_test = boundary_points(X_test_norm, NN)
+plt.contourf(xx_test, yy_test, Z_test, alpha=0.15, cmap='coolwarm')
+
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Test dataset classification', fontsize=14, fontweight="bold")
+plt.legend()
+plt.show()
+
+
+################################################
+#           EXERCISE 2                         #
+################################################
+
+# Class implementing standard gradient-descent (only hyper-parameter is the learning-rate)
+class OptimMom(object):
+    def __init__(self, learning_rate = .01, beta = .9):
+        self.lr = learning_rate
+        self.beta = beta
+        self.vt_previous = []
+    # receive the parameters of the MLP and the computed gradients and update the latter
+    def step(self,weight_list,gradient, iteration):
+        uw = []
+        self.vt_previous = []
+
+        if len(self.vt_previous) == 0:
+            for w in weight_list:
+                self.vt_previous.append(np.zeros(w.shape))
+        
+        i = 0
+        for w,grd in zip(weight_list,gradient):
+            vt = self.moving_average(grd, self.vt_previous[i]) 
+            
+            #FIXME: decide whether to use bias correction or not. 
+            vt = vt / (1 - self.beta**(iteration+1)) # bias correction
+            uw.append(w - self.lr * vt)
+            self.vt_previous[i] = vt
+            i += 1
+            
+        return uw
+
+    def moving_average(self, gradW, vt_prev):
+        # print("gradW", gradW)
+        return self.beta * vt_prev + (1 - self.beta) * gradW
+
+
+
+# Same training procedure than befor but using the optimizer class
+def train_woptimizer(NN,X, y,epoch = 5000,optim = None):
     list_loss = []
     
     for i in range(epoch):
@@ -77,24 +256,36 @@ def train(NN,X, y,epoch = 10000,lr = .1):
         yHat = NN.forward(X)
         
         gradW1,gradW2 = NN.backward(X,yHat,y)
-        #now update the weight using gradient descent
-        NN.W1 -= gradW1 * lr 
-        NN.W2 -= gradW2 * lr
+        #now update the weight using the optimizer class
+        NN.W1, NN.W2 = optim.step([NN.W1,NN.W2],[gradW1,gradW2], i)
         
         if i%100 == 0 : 
             loss = NN.loss(yHat,y)
-            print('Loss {}={}'.format(i,loss))
+            # print('Loss {}={}'.format(i,loss))
             list_loss.append(loss)
         
     return list_loss
 
-
-NN = MLP(hiddenNode=10)
+NN_momentum = MLP(hiddenNode=20)
 #Train network with the data:
-list_loss = train(NN, X_train, C_train, epoch=10000, lr=0.01)
+momentum_list_loss = train_woptimizer(NN_momentum, X_train_norm, C_train, epoch=10000, optim=OptimMom(learning_rate=0.1, beta=0.9))
 
-# Loss function iterations
-plt.plot(list_loss)
-plt.xlabel('Iterations')
-plt.ylabel('Loss Val')
+
+fig6 = plt.figure()
+
+plt.plot(momentum_list_loss, color="black", label="Momentum error")
+plt.plot(train_list_loss, color="red", label="No momentum")
+plt.title("Loss evolution using SGD with momentum", fontsize=14, fontweight="bold")
+plt.xlabel("Iterations")
+plt.ylabel("Loss value")
+plt.grid()
+plt.legend()
 plt.show()
+
+
+## Tests for different values of beta
+## We can do it this way since we are using a fixed random seed
+
+NN_momentum = MLP(hiddenNode=20)
+#Train network with the data:
+momentum_list_loss = train_woptimizer(NN_momentum, X_train_norm, C_train, epoch=10000, optim=OptimMom(learning_rate=0.1, beta=0.9))
---
+++ P1/questions.md
@@ -0,0 +1,9 @@
+# QUESTIONS LAB 1
+
+* Loss evaluated simultaneously on test and train?
+
+* Which is the proper value to init Vts?
+
+* Should we use the bias corrected version of Vt?
+
+* SGD with momentum does not work better than regular GD - Solved
