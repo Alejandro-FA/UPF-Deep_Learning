cfa9c5f548a2128b13582adb1f038a5a57946587
Alejandro-FA <61871580+Alejandro-FA@users.noreply.github.com>
2023-05-25 16:37:59 +0200

    Minor modifications to work with M1 GPU

--- P3/ex1.py
+++ P3/ex1.py
@@ -35,6 +35,8 @@ device = torch.device(
     'cpu'
 )
 
+print("Device selected:", device)
+
 
 """
 This piece of code is used to store the models for future reference, or to load
@@ -301,7 +303,7 @@ with open(results_path + "models/" + test_model_name + ".txt", "a") as results_t
     results_txt.write(f"Loss function: {criterion}\n")
     results_txt.write(f"Optimizer: {optimizer}\n")
     sys.stdout = results_txt # FIXME: This is 
-    torchsummary.summary(CNN, (3, 32, 32))
+    torchsummary.summary(CNN.to('cpu'), (3, 32, 32))
 
 
 sys.stdout = sys.__stdout__
